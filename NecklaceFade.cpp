4a945a889c24e92c73fdb751c2405ce16b29ac32
jldonovan3 <josephleodonovan@gmail.com>
2016-12-08 19:48:10 -0700

    1.2

--- NecklaceFade.cpp
+++ NecklaceFade.cpp
@@ -1,42 +1,51 @@
-    class Pulser
+class Pulse
+{
+	// Class Member Variables
+	// These are initialized at startup
+	int ledPin;      // the number of the LED pin
+	long OnTime;     // milliseconds of on-time
+	long OffTime;    // milliseconds of off-time
+ 
+	// These maintain the current state
+	int ledState;             		// ledState used to set the LED
+	unsigned long previousMillis;  	// will store last time LED was updated
+ 
+  // Constructor - creates a Flasher 
+  // and initializes the member variables and state
+  public:
+  Pulse(int pin, long on, long off)
+  {
+	ledPin = pin;
+	pinMode(ledPin, OUTPUT);     
+	  
+	OnTime = on;
+	OffTime = off;
+	
+	previousMillis = 0;
+  }
+ 
+  void Update()
+  {
+    // check to see if it's time to change the state of the LED
+    unsigned long currentMillis = millis();
+     
+    if((int ledState = 0 ; ledState <= 255; ledState += 5) && (currentMillis - previousMillis >= OnTime))
     {
-    	// Class Member Variables
-    	// These are initialized at startup
-    	int ledPin;      // the number of the LED pin
-    	int brightness;     // amount of brightness
-    	int fadeAmount;    // amount of fade effect
-     
-      // Constructor - creates a Pulser 
-      // and initializes the member variables and state
-      public:
-      Pulser(int pin, int brightValue, int fadeValue)
-      {
-    	ledPin = pin;
-    	pinMode(ledPin, OUTPUT);     
-    	  
-    	brightness = brightValue;
-    	fadeAmount = fadeValue;
-    	
-      }
-     
-      void Update()
-      {
-        analogWrite(ledPin, brightness);
-
-  // change the brightness for next time through the loop:
-        brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-        if (brightness <= 0 || brightness >= 255) {
-            fadeAmount = -fadeAmount;
-            }
-      }
-    };
+      previousMillis = currentMillis;  // Remember the time
+      analogWrite(ledPin, ledState);  // Update the actual LED
+    }
+    else if ((int ledState = 255 ; ledState >= 0; ledState -= 5) && (currentMillis - previousMillis >= OffTime))
+    {
+      previousMillis = currentMillis;   // Remember the time
+      analogWrite(ledPin, ledState);	  // Update the actual LED
+    }
+  }
+};
      
      
-    Pulser led1(5, 0, 5);
-    Pulser led2(6, 0, 5);
-    Pulser led2(7, 0, 5);
+    Pulser led1(5, 30, 30);
+    Pulser led2(6, 30, 30);
+    Pulser led2(7, 30, 30);
      
     void setup()
     {
@@ -46,5 +55,5 @@
     {
     	led1.Update();
     	led2.Update();
-        led3.Update();
+      led3.Update();
     }
