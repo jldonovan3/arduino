3fb3c6a175bb7d80ca0a099f8ba6d34af821fb9a
jldonovan3 <josephleodonovan@gmail.com>
2022-06-17 12:54:11 -0400

    Add files via upload

---
+++ 3LEDButton.ino
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+
+int switchState = 0; //define empty variable for the power state of the button
+
+void setup()
+{
+  // Initialize pins
+  pinMode(3, OUTPUT); //green LED
+  pinMode(4, OUTPUT); //red LED
+  pinMode(5, OUTPUT); //red LED
+  pinMode(2, INPUT); //button
+
+}
+
+void loop()
+{
+
+  switchState = digitalRead(2); //Read the power from the button pin
+  
+  if (switchState == LOW) //if the button is not pressed, only fire the green LED
+  {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }
+  else 
+  { 
+    digitalWrite(3, LOW); //turn off green LED if the button is pressed
+    
+    digitalWrite(4, LOW); //turn off the red LED on pin 4
+    digitalWrite(5, HIGH); //rapid-blink the red LED on pin 5
+    delay(70);
+    digitalWrite(5, LOW);
+    delay(70);
+    digitalWrite(5, HIGH);
+    delay(70);
+    digitalWrite(5, LOW);
+    delay(70);
+    digitalWrite(5, HIGH);
+    delay(70);
+    digitalWrite(5, LOW);
+    
+    delay(100); //pause between red LED alternation
+    
+    digitalWrite(5, LOW); //turn off the red LED on pin 5
+    digitalWrite(4, HIGH); //rapid-blink the red LED on pin 4
+    delay(70);
+    digitalWrite(4, LOW);
+    delay(70);
+    digitalWrite(4, HIGH);
+    delay(70);
+    digitalWrite(4, LOW);
+    delay(70);
+    digitalWrite(4, HIGH);
+    delay(70);
+    digitalWrite(4, LOW);
+    
+    delay(100);
+  }
+}
---
+++ Blink.ino
@@ -0,0 +1,36 @@
+#include <Adafruit_NeoPixel.h>
+
+int PIN = 6;
+int totalLEDs = 64;
+int ledFadeTime = 5;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(totalLEDs, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.setBrightness(255);
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  rgbFadeInAndOut(200, 200, 255, ledFadeTime); // Blue
+}
+
+void rgbFadeInAndOut(uint8_t red, uint8_t green, uint8_t blue, uint8_t wait) {
+  for(uint8_t b = 25; b <255; b++) {
+     for(uint8_t i=0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, red * b/255, green * b/255, blue * b/255);
+     }
+
+     strip.show();
+     delay(wait);
+  };
+
+  for(uint8_t b=255; b > 25; b--) {
+     for(uint8_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, red * b/255, green * b/255, blue * b/255);
+     }
+     strip.show();
+     delay(wait);
+  };
+};
